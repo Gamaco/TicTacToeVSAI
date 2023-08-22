d9949d588a28ca3066dbba6d0113aff5354e180d
gmaldon2 <gabrielmacolon@gmail.com>
2023-08-22 17:15:40 -0400

    Re-structured the function "checkForWinner()" using pre defined patterns instead of iterating through positions on the board.

--- JavaScript.js
+++ JavaScript.js
@@ -1,19 +1,29 @@
+
 window.onload = () => {
     createBoard();
 }
 
+
+// Variables
 const PLAYER = 0;
 const BOT = 1;
 
 var rows = 3;
 var columns = 3;
-var offset = 3; //Numero de columnas.
+var offset = 3; //Amount of rows represented in the array.
 
 var board = [];
+var gameOver = false;
+var turn = Math.floor(Math.random());
 
-var turn = Math.round(Math.random());
 
 
+// The purpose of this function is to initialize the table 
+// array with a character to indicate that the positions are
+// empty.
+
+// Since the first turn to play the game is randomly selected, 
+// we also check if the current turn is the bot to make a move.
 function createBoard() {
     for (let row = 0; row < rows; row++) {
         for (let column = 0; column < columns; column++) {
@@ -22,12 +32,19 @@ function createBoard() {
     }
 
     if (turn == BOT) {
-        playBotPosition();
+        playPosition();
     }
 }
 
 
-function selectPosition(square) {
+// The purpose of this function is to play a position on the board.
+// It handles both player and bot moves on the board.
+function playPosition(square) {
+
+// Player is unable to continue selecting 
+// positions on the board if the game is over.  ¯\_(ツ)_/¯ 
+if (gameOver == true) return -1;
+
     if (turn == PLAYER) {
         let coords = square.id.split("-");
         let row = parseInt(coords[0]);
@@ -41,56 +58,80 @@ function selectPosition(square) {
             square.innerText = "X";
             board[index] = "X";
 
-            turn = BOT;
-
-            let winner = checkForWinner();
-
-            if (winner == -1) {
-                playBotPosition();
-            }
         }
         else {
             alert("Select another position to play. This position is not available.");
         }
     }
+
+    if (turn == BOT) {
+        
+        let positions = getAvailablePos(); // Multi-array of positions available. E.g [row, column].
+
+        // In case the array is empty, it'll show a message. 
+        if (positions == -1) {
+            alert("There's no more playable positions. Game over.");
+            gameOver = true;
+        }
+        else {
+            // Selects a random position from the options available.
+            let randomIndex = Math.floor(Math.random() * positions.length);
+            let selectedBotPosition = positions[randomIndex];
+
+            // These row and column vars are being used to get the element ID,
+            // in order to change the visuals of the game board: X and O
+            let row = selectedBotPosition[0];
+            let column = selectedBotPosition[1];
+
+            // The index is being used to change the value within the table array itself.
+            let index = getBoardIndex(row, column);
+
+            board[index] = "O";
+
+            // Display the selected position. 
+            let square = document.getElementById(row.toString() + "-" + column.toString());
+            square.innerText = "O";
+        }
+    }
+
+    gameOverCheck();
 }
 
 
+
+// This function verifies if the game is over by checking if there's a winner.
+// Otherwise, it changes the player's corresponding turn.
+function gameOverCheck() {
+    if (checkForWinner() == -1) {
+        if (turn == PLAYER) {
+            turn = BOT;
+            playPosition();
+        }
+        else {
+            turn = PLAYER;
+        }
+    }
+    else {
+        gameOver = true;
+        let winner = (turn == PLAYER) ? "You" : "Bot (AI)";
+        alert(winner + " Won! Game over.");
+    }
+}
+
+
+// As previously discussed in class, this function -
+// does the convertion from a row and column position to 
+// a single index position within the table array.
 function getBoardIndex(row, column) {
     return column + row * offset;
 }
 
 
-function playBotPosition() {
-
-    let availablePositions = getPositionAvailable();
-
-    if (availablePositions == -1) {
-        alert("There's no more playable positions. Game over.");
-    }
-    else {
-        let randomIndex = Math.floor(Math.random() * availablePositions.length);
-        let selectedBotPosition = availablePositions[randomIndex];
-
-        let row = selectedBotPosition[0];
-        let column = selectedBotPosition[1];
-        let index = getBoardIndex(row, column);
-
-        board[index] = "O";
-
-        let square = document.getElementById(row.toString() + "-" + column.toString());
-        square.innerText = "O";
-
-        let winner = checkForWinner();
-
-        if (winner == -1) {
-            turn = PLAYER;
-        }
-    }
-}
-
-
-function getPositionAvailable() {
+// Returns a multi-dimensional array of 
+// available positions, each array contains 
+// the row and column of each available position.
+// In case there's no position available, it returns -1.
+function getAvailablePos() {
     let positions = [];
     for (let row = 0; row < rows; row++) {
         for (let column = 0; column < columns; column++) {
@@ -100,71 +141,36 @@ function getPositionAvailable() {
             }
         }
     }
-
-    if (positions.length < 1) {
-        return -1;
-    }
-    else {
-        return positions;
-    }
+    return (positions.length < 1) ? -1 : positions;
 }
 
 
+// This function returns 1 if there's a winning pattern, 
+// and -1 if there's no winning pattern found.
 function checkForWinner() {
-    //Horizontal Check
-    for (let row = 0; row < rows; row++) {
-        let column = 0;
-        if (board[getBoardIndex(row, column)] == board[getBoardIndex(row, column + 1)] && board[getBoardIndex(row, column + 1)] == board[getBoardIndex(row, column + 2)]) {
-            let symbol = board[getBoardIndex(row, column)];
-            if (symbol == "X") {
-                highlightPositions("horizontal", row, column);
-                return 1;
-            }
-            if (symbol == "O") {
-                highlightPositions("horizontal", row, column);
-                return 1;
-            }
-        }
-    }
-    //Vertical Check
-    for (let column = 0; column < columns; column++) {
-        let row = 0;
-        if (board[getBoardIndex(row, column)] == board[getBoardIndex(row + 1, column)] && board[getBoardIndex(row + 1, column)] == board[getBoardIndex(row + 2, column)]) {
-            let symbol = board[getBoardIndex(row, column)];
-            if (symbol == "X") {
-                highlightPositions("vertical", row, column);
-                return 1;
-            }
-            if (symbol == "O") {
-                highlightPositions("vertical", row, column);
-                return 1;
-            }
-        }
-    }
-    
-    if (board[getBoardIndex(0,0)] == board[getBoardIndex(1,1)] && board[getBoardIndex(1,1)] == board[getBoardIndex(2,2)]) {
-        let symbol = board[getBoardIndex(0, 0)];
-        if (symbol == "X") {
-            highlightPositions("diagonal1", 0, 0);
-            return 1;
-        }
-        if (symbol == "O") {
-            highlightPositions("diagonal1", 0, 0);
-            return 1;
-        }
-    }
-    if (board[getBoardIndex(2,0)] == board[getBoardIndex(1,1)] && board[getBoardIndex(1,1)] == board[getBoardIndex(0,2)]) {
-        let symbol = board[getBoardIndex(2, 0)];
-        if (symbol == "X") {
-            highlightPositions("diagonal2", 2, 0);
-            return 1;
-        }
-        if (symbol == "O") {
-            highlightPositions("diagonal2", 2, 0);
+    let winningPatterns = [
+        // Rows
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        // Columns
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        // Diagonal left to right
+        [0, 4, 8],
+        // Diagonal right to left
+        [2, 4, 6]
+    ];
+
+    for (let arr of winningPatterns) {
+
+        // Current array in the loop.
+        let [a, b, c] = arr;
+
+        // Checks if all the positions' values are the same and returns 1.
+        if (board[a] != "-" && board[a] == board[b] && board[a] == board[c]) {
             return 1;
         }
     }
 
+    // In case it couldn't find a winning pattern, it returns -1.
     return -1;
 }
 
@@ -189,7 +195,7 @@ function highlightPositions(direction, row, column) {
     else {
         var box1 = document.getElementById(row.toString() + "-" + column.toString());
         var box2 = document.getElementById((row - 1).toString() + "-" + (column + 1).toString());
-        var box3 = document.getElementById((row - 2).toString() + "-" + (column + 2).toString());   
+        var box3 = document.getElementById((row - 2).toString() + "-" + (column + 2).toString());
     }
 
     box1.style.color = "lightgreen";
